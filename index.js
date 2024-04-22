9f62457c0f8c312b3fe935480edbc0231fc1ea19
odilov-a <akbarjonodilov01@gmail.com>
2024-04-22 10:48:19 +0500

    Update file paths in index.js

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/html/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/home", (req, res) => {
-  res.sendFile(__dirname + "/html/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/blog", (req, res) => {
-  res.sendFile(__dirname + "/html/full-width.html");
+  res.sendFile(__dirname + "/full-width.html");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(__dirname + "/html/about.html");
+  res.sendFile(__dirname + "/about.html");
 });
 
 app.get("/contact", (req, res) => {
-  res.sendFile(__dirname + "/html/contact.html");
+  res.sendFile(__dirname + "/contact.html");
 });
 
 
 app.use(express.static(path.join(__dirname, "public")));
-app.listen(3000, () => {
-  console.log(`http://localhost:3000`);
+app.listen(3333, () => {
+  console.log(`http://localhost:3333`);
 });
